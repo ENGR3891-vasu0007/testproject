181b88f5f1c49a865465b914343289a7ccacb510
ENGR3891-vasu0007 <80737532+ENGR3891-vasu0007@users.noreply.github.com>
2021-10-25 13:04:15 +1030

    Add files via upload

---
+++ DBConnect.php
@@ -0,0 +1,19 @@
+<?php
+
+#DBConnect.php
+$host = 'mytestserver1.mysql.database.azure.com';
+$username = 'vasulg';
+$password = 'Flinders@';
+$db_name = 'comp9710';
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn, NULL, NULL, "cacert.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno($conn)) {
+    die('Failed to connect to MySQL: ' . mysqli_connect_error());
+} 
+?>
