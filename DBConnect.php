a4a6f480e52e4935bc2f1f2ec8105b86820352e7
ENGR3891-vasu0007 <80737532+ENGR3891-vasu0007@users.noreply.github.com>
2021-10-25 13:46:41 +1030

    Update DBConnect.php

--- DBConnect.php
+++ DBConnect.php
@@ -7,13 +7,26 @@ $password = 'Flinders@';
 $db_name = 'comp9710';
 $conn = mysqli_init();
 
-mysqli_ssl_set($conn, NULL, NULL, "cacert.pem", NULL, NULL);
+mysqli_ssl_set($conn, NULL, NULL, ""/var/www/html/DigiCertGlobalRootG2.crt.pem"", NULL, NULL);
 
 // Establish the connection
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 //If connection failed, show the error
-if (mysqli_connect_errno($conn)) {
+if (mysqli_connect_errno()) {
     die('Failed to connect to MySQL: ' . mysqli_connect_error());
 } 
+
+// Run the create table query
+if (mysqli_query($conn, '
+CREATE TABLE Products (
+`Id` INT NOT NULL AUTO_INCREMENT ,
+`ProductName` VARCHAR(200) NOT NULL ,
+`Color` VARCHAR(50) NOT NULL ,
+`Price` DOUBLE NOT NULL ,
+PRIMARY KEY (`Id`)
+);
+')) {
+printf("Table created\n");
+}
 ?>
